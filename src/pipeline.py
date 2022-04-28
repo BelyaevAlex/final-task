486692edd0ad75b9345f798ba1ad0478990ed0fc
BelyaevAlex <77803344+BelyaevAlex@users.noreply.github.com>
2022-04-28 19:07:14 +0300

    Add files via upload

---
+++ src/pipeline.py
@@ -0,0 +1,11 @@
+from sklearn.preprocessing import StandardScaler
+
+
+def create_pipeline(use_scaler: bool, max_iter: int, random_state: int) -> Pipeline:
+    pipeline_steps = []
+    if use_scaler:
+        pipeline_steps.append(("scaler", StandardScaler()))
+    pipeline_steps.append(
+        ("classifier", LogisticRegression(random_state=random_state, max_iter=max_iter))
+    )
+    return Pipeline(steps=pipeline_steps)
