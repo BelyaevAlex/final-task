b706060f6a9cd2c1d30d68efc09775036c55ef5e
BelyaevAlex <77803344+BelyaevAlex@users.noreply.github.com>
2022-04-28 23:17:05 +0300

    Add files via upload

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,11 +1,12 @@
 from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LogisticRegression
 
-
-def create_pipeline(use_scaler: bool, max_iter: int, random_state: int) -> Pipeline:
+def create_pipeline(use_scaler: bool, max_iter: int, penalty: str) -> Pipeline:
     pipeline_steps = []
     if use_scaler:
         pipeline_steps.append(("scaler", StandardScaler()))
     pipeline_steps.append(
-        ("classifier", LogisticRegression(random_state=random_state, max_iter=max_iter))
+        ("classifier", LogisticRegression(max_iter=max_iter, penalty=penalty))
     )
     return Pipeline(steps=pipeline_steps)
--- src/train.py
+++ src/train.py
@@ -1,10 +1,11 @@
-import numpy as np
-import pandas as pd
 from pathlib import Path
 import click
+from joblib import dump
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+from .pipeline import create_pipeline
+from .database import get_dataset
 
 @click.command()
 @click.option(
@@ -14,13 +15,57 @@ from sklearn.metrics import accuracy_score
     type=click.Path(exists=True, dir_okay=False, path_type=Path)
 )
 
+@click.option(
+    "-p",
+    "--penalty",
+    default="l2",
+    type=str
+)
 
+@click.option(
+    "-m",
+    "--max_iter",
+    default=100,
+    type=int
+)
+@click.option(
+    "-s",
+    "--save-model-path",
+    default="data/model.joblib",
+    type=click.Path(dir_okay=False, writable=True, path_type=Path),
+)
+@click.option(
+    "-u",
+    "--use_scaler",
+    default=True,
+    type=bool,
+)
+@click.option(
+    "-sd",
+    "--split_dataset",
+    default=False,
+    type=bool,
+)
+@click.option(
+    "-t",
+    "--test_size",
+    default=0.2,
+    type=float,
+)
 def train(
-    dataset_path: Path
+    dataset_path: Path,
+    save_model_path: Path,
+    use_scaler: bool,
+    test_size: float,
+    split_dataset: bool,
+    penalty: str,
+    max_iter: int
 ):
-    data = pd.read_csv(dataset_path)
-    print(data.columns)
-    x = data.copy().drop(columns='Cover_Type')
-    y = data['Cover_Type']
-    model = LogisticRegression()
-    model.fit(x, y)
+    pipeline = create_pipeline(use_scaler, max_iter, penalty)
+    if split_dataset:
+        x, x_test, y, y_test = get_dataset(dataset_path, split_dataset, test_size)
+    else:
+        x, y = get_dataset(dataset_path, split_dataset, test_size)
+    pipeline.fit(x, y)
+    dump(pipeline, save_model_path)
+    print(f'Model was save in {save_model_path}')
