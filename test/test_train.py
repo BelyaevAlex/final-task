2913961a60505673da490afd156a794286e1a009
BelyaevAlex <77803344+BelyaevAlex@users.noreply.github.com>
2022-05-03 20:12:09 +0300

    Add files via upload

--- test/test_train.py
+++ test/test_train.py
@@ -3,6 +3,7 @@ import pytest
 import joblib
 import sklearn
 import os
+import pandas as pd
 from src.train import train
 from pathlib import Path
 
@@ -43,7 +44,6 @@ def test_save_model(
     path_cd = os.getcwd()
     os.makedirs("test_model/")
     runner = CliRunner()
-    print(Path(path_cd + '/test_model/'))
     path = Path(path_cd + '/test_model/model.joblib')
     result = runner.invoke(
         train,
@@ -56,3 +56,62 @@ def test_save_model(
     os.remove(path)
     os.rmdir(path_cd + '/test_model/')
     assert str(type(model)) == "<class 'sklearn.pipeline.Pipeline'>"
+
+def test_input_data(
+) -> None:
+    data = [[13457, 2696, 110, 8, 60, 300, 60, 230, 234,
+             138, 828, 0, 0, 1, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 1, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 5],
+            [13458, 2335, 27, 29, 30, 12, 451, 194, 163,
+             90, 666, 0, 0, 0, 1, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 1, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 6],
+            [13459, 2437, 66, 25, 30, -5, 824, 235, 182,
+             64, 859, 0, 0, 0, 1, 1, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 3],
+            [13460, 2416, 53, 24, 0, 0, 774, 226, 188,
+             85, 886, 0, 0, 0, 1, 1, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 4]]
+    data = pd.DataFrame(data, columns=['Id', 'Elevation', 'Aspect', 'Slope',
+       'Horizontal_Distance_To_Hydrology', 'Vertical_Distance_To_Hydrology',
+       'Horizontal_Distance_To_Roadways', 'Hillshade_9am', 'Hillshade_Noon',
+       'Hillshade_3pm', 'Horizontal_Distance_To_Fire_Points',
+       'Wilderness_Area1', 'Wilderness_Area2', 'Wilderness_Area3',
+       'Wilderness_Area4', 'Soil_Type1', 'Soil_Type2', 'Soil_Type3',
+       'Soil_Type4', 'Soil_Type5', 'Soil_Type6', 'Soil_Type7', 'Soil_Type8',
+       'Soil_Type9', 'Soil_Type10', 'Soil_Type11', 'Soil_Type12',
+       'Soil_Type13', 'Soil_Type14', 'Soil_Type15', 'Soil_Type16',
+       'Soil_Type17', 'Soil_Type18', 'Soil_Type19', 'Soil_Type20',
+       'Soil_Type21', 'Soil_Type22', 'Soil_Type23', 'Soil_Type24',
+       'Soil_Type25', 'Soil_Type26', 'Soil_Type27', 'Soil_Type28',
+       'Soil_Type29', 'Soil_Type30', 'Soil_Type31', 'Soil_Type32',
+       'Soil_Type33', 'Soil_Type34', 'Soil_Type35', 'Soil_Type36',
+       'Soil_Type37', 'Soil_Type38', 'Soil_Type39', 'Soil_Type40',
+       'Cover_Type'])
+    data.to_csv('train.csv')
+    runner = CliRunner()
+    path = os.getcwd() + '/train.csv'
+    result = runner.invoke(
+        train,
+        [
+            '--dataset-path',
+            path
+        ],
+    )
+    assert result.exit_code == 0
+
